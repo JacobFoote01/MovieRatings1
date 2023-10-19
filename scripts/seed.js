1613f7abf0d8350686e4f5716c8efa32e6b6d386
Jacob Foote <jacobjfoote01@gmail.com>
2023-10-18 20:33:53 -0600

    commit

---
+++ scripts/seed.js
@@ -0,0 +1,40 @@
+import { Movie, Rating, User, db } from '../src/model.js';
+import movieData from './data/movies.json' assert { type: 'json' };
+
+console.log('Syncing database...');
+const usersInDB = await Promise.all(usersToCreate);
+
+console.log(usersInDB);
+await db.sync({ force: true });
+
+console.log('Seeding database...');
+
+const moviesInDB = await Promise.all(
+    movieData.map((movie) => {
+    const releaseDate = new Date(Date.parse(movie.releaseDate))
+    const {title, overview, posterPath} = movie
+
+    const newMovie = Movie.create({
+        title,
+        overview,
+        posterPath,
+        releaseDate,
+    })
+    return newMovie
+}),
+)
+
+const ratingsInDB = await Promise.all(
+    usersInDB.flatMap((user) => {
+      const randomMovies = lodash.sampleSize(moviesInDB, 10);
+      const movieRatings = randomMovies.map((movie) => {
+        return Rating.create({
+          score: lodash.random(1, 5),
+          userId: user.userId,
+          movieId: movie.movieId,
+        });
+      });
+      return movieRatings;
+    }),
+  );
+  console.log(ratingsInDB);
